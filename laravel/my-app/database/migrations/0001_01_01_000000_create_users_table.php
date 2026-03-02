90e0350d3badacf6c765988ef8cad9d2f5414eb3
pdilip84 <parmardilip2013@gmail.com>
2026-03-02 09:58:25 +0530

    some basics about route & factory

--- laravel/my-app/database/migrations/0001_01_01_000000_create_users_table.php
+++ laravel/my-app/database/migrations/0001_01_01_000000_create_users_table.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration
 {
     /**
-     * Run the migrations.
+     * Run the migrations. php artisan migrate
      */
     public function up(): void
     {
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('is_premium')->default(false);
             $table->rememberToken();
             $table->timestamps();
         });
@@ -38,7 +39,7 @@ return new class extends Migration
     }
 
     /**
-     * Reverse the migrations.
+     * Reverse the migrations. php artisan migrate:rollback
      */
     public function down(): void
     {
--- laravel/my-app/database/seeders/DatabaseSeeder.php
+++ laravel/my-app/database/seeders/DatabaseSeeder.php
@@ -22,6 +22,10 @@ class DatabaseSeeder extends Seeder
             'email' => 'test@example.com',
         ]);
 
+        User::factory(3)->unverified()->create();
+
+        User::factory(2)->premium()->create();
+
         $this->call(MemberSeeder::class);
     }
 }
