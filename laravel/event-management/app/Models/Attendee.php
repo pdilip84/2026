0e3ac38f234278b8b22fad43d3e01b5b0faf480f
pdilip84 <parmardilip2013@gmail.com>
2026-04-30 13:50:22 +0530

    Starting with api project event-management

---
+++ laravel/event-management/app/Models/Attendee.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Attendee extends Model
+{
+    public function event(): BelongsTo
+    {
+        return $this->belongsTo(Event::class);
+    }
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ laravel/event-management/app/Models/Event.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Event extends Model
+{
+    public function attendees(): HasMany
+    {
+        return $this->hasMany(Attendee::class);
+    }
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
